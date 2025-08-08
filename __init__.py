14af14bed13c7ad10da2de5be004ab486c9fae83
Intelektika <kvantorium73.int@gmail.com>
2025-08-08 23:48:18 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-# empty
+from main import nelin_layer, nelin_models, nelin_text, nlarr
+from models import nelin_fortest
+from text import nelin_chiper
+from functs import generate_xy, softmax, softsign, sigmoid, swish, relu, leaky_relu, tanh
+from main import greet
+import numpy as np
